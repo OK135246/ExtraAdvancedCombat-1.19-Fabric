0304b49c347300c9552c150072cc7f46d2eef9f6
OK135246 <111523502+OK135246@users.noreply.github.com>
2022-09-05 18:51:55 +0100

    axes added

--- src/main/java/net/OK135246/extraadvancedcombat/item/ModItems.java
+++ src/main/java/net/OK135246/extraadvancedcombat/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.OK135246.extraadvancedcombat.item;
 
 import net.OK135246.extraadvancedcombat.ExtraAdvancedCombat;
+import net.OK135246.extraadvancedcombat.item.custom.ModAxeItem;
 import net.OK135246.extraadvancedcombat.item.custom.ModPickaxeItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.*;
@@ -51,6 +52,46 @@ public class ModItems {
 
     public static final Item ADVANCED_NETHER_STAR_PICKAXE = registerItem("advanced_nether_star_pickaxe",
             new ModPickaxeItem(ModToolMaterials.ADVANCED_NETHER_STAR, 1, -2.6f,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+    public static final Item ADVANCED_WOODEN_AXE = registerItem("advanced_wooden_axe",
+            new ModAxeItem(ModToolMaterials.ADVANCED_WOOD, 10, -2.8f,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+    public static final Item ADVANCED_STONE_AXE = registerItem("advanced_stone_axe",
+            new ModAxeItem(ModToolMaterials.ADVANCED_STONE, 10, -2.8f,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+    public static final Item ADVANCED_IRON_AXE = registerItem("advanced_iron_axe",
+            new ModAxeItem(ModToolMaterials.ADVANCED_IRON, 10, -2.8f,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+    public static final Item ADVANCED_GOLD_AXE = registerItem("advanced_gold_axe",
+            new ModAxeItem(ModToolMaterials.ADVANCED_GOLD, 10, -2.8f,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+    public static final Item ADVANCED_DIAMOND_AXE = registerItem("advanced_diamond_axe",
+            new ModAxeItem(ModToolMaterials.ADVANCED_DIAMOND, 10, -2.8f,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+    public static final Item EMERALD_AXE = registerItem("emerald_axe",
+            new ModAxeItem(ModToolMaterials.EMERALD, 10, -2.8f,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+    public static final Item ADVANCED_EMERALD_AXE = registerItem("advanced_emerald_axe",
+            new ModAxeItem(ModToolMaterials.ADVANCED_EMERALD, 10, -2.8f,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+    public static final Item ADVANCED_NETHERITE_AXE = registerItem("advanced_netherite_axe",
+            new ModAxeItem(ModToolMaterials.ADVANCED_NETHERITE, 10, -2.8f,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+    public static final Item NETHER_STAR_AXE = registerItem("nether_star_axe",
+            new ModAxeItem(ModToolMaterials.NETHER_STAR, 10, -2.8f,
+                    new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+    public static final Item ADVANCED_NETHER_STAR_AXE = registerItem("advanced_nether_star_axe",
+            new ModAxeItem(ModToolMaterials.ADVANCED_NETHER_STAR, 10, -2.8f,
                     new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(ExtraAdvancedCombat.MOD_ID, name), item);
